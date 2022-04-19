6f6071e9ffcb230124deea84704f99803facb3ec
TheInLimE <104007417+TheInLimE@users.noreply.github.com>
2022-04-19 14:41:46 +0200

    Test
    Testing file

---
+++ dormsim.py
@@ -0,0 +1,107 @@
+import time
+print("welcome to college dorm simulator from the developers of screwsloose software")
+time.sleep(6)
+print("you wake up in the middle of the night")
+time.sleep(3)
+print("you hear loud breathing and grunting noises from the next room what will you do?")
+print("a=go inside")
+time.sleep(2)
+print("b=dont go inside")
+time.sleep(2)
+print("c=go back to sleep")
+answer = input()
+if answer == "a":
+    print("you go inside your room mates room and........ get stabbed")
+    time.sleep(4)
+    exit()
+if answer == "b":
+    print("you decide not go into your roommates room what will you do now")
+    time.sleep(5)
+    print("a=go to the college cafeteria")
+    print("b=go to the bathroom")
+    print("c=go to the dorm next door")
+if answer == "c":
+    time.sleep(1)
+    print("you go back asleep")
+    time.sleep(3)
+    exit()
+answer = input()
+if answer == "a":
+    time.sleep(1)
+    print("you go to the school cafeteria and you hear a weird criiking noise from the closet and ....... you died due"
+          "to a stabbing")
+    time.sleep(5)
+    exit()
+
+if answer == "b":
+    time.sleep(1)
+    print("you go to the bathroom, and your roommate barricades you trapping you into the bathroom for ever")
+    time.sleep(6)
+    exit()
+
+if answer == "c":
+    print("you knock on the dorm next door they wont open")
+    time.sleep(4)
+    print("you are now locked from your own dorm room, what now")
+    time.sleep(3)
+    print("what will you do")
+    time.sleep(2)
+    print("a=go talk to the dean")
+    print("b=sit there and wait")
+answer = input()
+if answer == "a":
+    time.sleep(1)
+    print("the dean doesnt open and you get stabbed")
+    time.sleep(3)
+    exit()
+if answer == "b":
+    print("you sit there and wait until, you see a dark figurine with a bloody knife coming towards you")
+    time.sleep(4)
+    print("you start running and the shadowy figurine starts running after you, you come to a T shaped turn")
+    time.sleep(4)
+    print("a=pick left")
+    print("b=pick right")
+answer = input()
+if answer == "a":
+    print("you pick left")
+    time.sleep(3)
+    print("its a dead end...... you die")
+    time.sleep(3)
+    exit()
+if answer == "b":
+    print("you turn left")
+    time.sleep(2)
+    print("you find a baseball bat made of iron")
+    time.sleep(3)
+    print("you pick up the bat and start to go offense")
+    time.sleep(3)
+    print("where will you hit the figurine")
+    print("a=leg")
+    print("b=liver")
+    print("c=head")
+    print("torso")
+answer = input()
+if answer == "a":
+    time.sleep(1)
+    print("you hit the person in the leg but you miss and get stabbed.on the bright side your school got into the news")
+    time.sleep(5)
+    exit()
+if answer == "b":
+    time.sleep(1)
+    print("you hit him in the liver and it is very effective and the police arrest him"
+          "and you because schools dont allow self defense lol")
+    time.sleep(7)
+    exit()
+if answer == "c":
+    time.sleep(1)
+    print("you hit the figurine in the head and he dies, later when the cops arrive you get arrested")
+    time.sleep(6)
+    exit()
+if answer == "d":
+    time.sleep(1)
+    print("you try to hit the figurine in the torso but you miss, the figurine says -should have gone for the head"
+          "then he stabs you")
+    time.sleep(7)
+    exit()
+
+
